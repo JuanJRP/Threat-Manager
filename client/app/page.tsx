8c781097307e25ebdda7d415c2c68f1eecdd010c
David Chica <david.chikhb@gmail.com>
2024-10-03 16:06:15 -0500

    Client structure created
    Color palette added, Barlow font added

---
+++ client/app/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-cPurple-500 bg-cPurple-950">Hello</h1>
+    </div>
+  );
+}
---
+++ client/tailwind.config.ts
@@ -0,0 +1,30 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        cPurple: {
+          50: "#fbf6fe",
+          100: "#f5eafd",
+          200: "#ecd9fb",
+          300: "#debaf8",
+          400: "#c98ef2",
+          500: "#af59e8",
+          600: "#a042db",
+          700: "#8a31bf",
+          800: "#742c9d",
+          900: "#5f257e",
+          950: "#410f5c"
+        }
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
