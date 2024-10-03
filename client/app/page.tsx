8f805e4b846a4fe161171624d7097af653f7afcd
David Chica <david.chikhb@gmail.com>
2024-10-03 17:52:32 -0500

    Test
    ALo

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <div>
-      <h1 className="text-cPurple-500 bg-cPurple-950">Hello</h1>
+      <h1 className="text-cPurple-500 bg-cPurple-950">Hello world</h1>
     </div>
   );
 }
